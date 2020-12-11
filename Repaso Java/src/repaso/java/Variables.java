cf2c399cbdb16808beb9cff245fbd4609bf60577
Antonio Teixeira <dam12029@cifpcarballeira.es>
2020-12-11 21:15:44 +0100

    Segundo Commit: Creacion de Variables, para practicar la asignacion de valores a variables y mostrar datos por pantalla

---
+++ Repaso Java/src/repaso/java/Variables.java
@@ -0,0 +1,18 @@
+
+package repaso.java;
+
+
+public class Variables {
+    public static void main(String[] args) {
+        int a=1;
+        String b="hola";
+        double c=3.5;
+        boolean d=false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+   
+}
